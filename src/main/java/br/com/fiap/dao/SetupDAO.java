a1ccb49d68125824497040f8dbafd787dad7959b
CarvalhoGabriell <gabriel.fernandes2312@gmail.com>
2021-04-25 16:10:14 -0300

    Novo metodo para buscar e listar todos os dados do banco

--- src/main/java/br/com/fiap/dao/SetupDAO.java
+++ src/main/java/br/com/fiap/dao/SetupDAO.java
@@ -1,22 +1,30 @@
 package br.com.fiap.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import br.com.fiap.model.Setup;
+import br.com.fiap.utils.EntityManagerFacede;
 
 public class SetupDAO {
+	private EntityManager manager= EntityManagerFacede.getEntityManager();
 	
 	public void save(Setup setup) {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("progamer-persistence");
-		EntityManager manager = factory.createEntityManager();
 		
 		manager.getTransaction().begin();
 		manager.persist(setup);
 		manager.getTransaction().commit();
-		
 		manager.close();
+	}
+	
+	// pegar todos os setups cadastrados no banco
+	public List<Setup> getAll() {
+		
+		String strSQL = "SELECT s FROM Setup s";
+		TypedQuery<Setup> query = manager.createQuery(strSQL, Setup.class);
+		return query.getResultList();
 	}
 	
 }
--- src/main/java/br/com/fiap/dao/UsuarioDAO.java
+++ src/main/java/br/com/fiap/dao/UsuarioDAO.java
@@ -1,22 +1,26 @@
 package br.com.fiap.dao;
 
+import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
+import javax.persistence.TypedQuery;
 import br.com.fiap.model.Usuario;
+import br.com.fiap.utils.EntityManagerFacede;
 
 public class UsuarioDAO {
 	
-	
+	private EntityManager manager= EntityManagerFacede.getEntityManager();
 	public void saveUser(Usuario user) {
 		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("progamer-persistence");
-		EntityManager manager = factory.createEntityManager();
 		manager.getTransaction().begin();
 		manager.persist(user);
 		manager.getTransaction().commit();
-		
 		manager.close();
+	}
+	
+	public List<Usuario> getAllUsers() {
+		
+		String strSQL = "SELECT s FROM Usuario s";
+		TypedQuery<Usuario> query = manager.createQuery(strSQL, Usuario.class);
+		return query.getResultList();
 	}
 }
