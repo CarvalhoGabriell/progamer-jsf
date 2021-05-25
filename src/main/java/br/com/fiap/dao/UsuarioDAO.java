f7567c41a21b3cba269304338c7dcc65f1e54864
CarvalhoGabriell <gabriel.fernandes2312@gmail.com>
2021-05-25 20:20:18 -0300

    Novos metodos de CRUD do DAO

--- src/main/java/br/com/fiap/dao/UsuarioDAO.java
+++ src/main/java/br/com/fiap/dao/UsuarioDAO.java
@@ -1,8 +1,10 @@
 package br.com.fiap.dao;
 
 import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+
 import br.com.fiap.model.Usuario;
 import br.com.fiap.utils.EntityManagerFacede;
 
@@ -22,5 +24,30 @@ public class UsuarioDAO {
 		String strSQL = "SELECT s FROM Usuario s";
 		TypedQuery<Usuario> query = manager.createQuery(strSQL, Usuario.class);
 		return query.getResultList();
+	}
+
+	public Usuario findById(Long id) {
+		
+		return manager.find(Usuario.class, id);
+	}
+	
+	public void updateUser(Usuario user) {
+		
+		manager.getTransaction().begin();
+		manager.merge(user);
+		manager.flush();
+		manager.getTransaction().commit();
+		manager.close();
+	}
+	
+	public void delete(Long id) {
+		
+		manager.getTransaction().begin();
+		Usuario user = manager.find(Usuario.class, id);
+		manager.remove(user);
+		System.out.println("Deletando Usuario ID "+user.getId()+" --- NOME: "+user.getNome()+" EMAIL: "+user.getEmail());
+		
+		manager.getTransaction().commit();
+		manager.close();
 	}
 }
