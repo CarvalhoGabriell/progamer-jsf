71c2dcffbd0867cdbdfae971acd90964b641ae1c
CarvalhoGabriell <gabriel.fernandes2312@gmail.com>
2021-05-28 19:13:53 -0300

    Alterando metodos de DELETE e UPDATE

--- src/main/java/br/com/fiap/dao/SetupDAO.java
+++ src/main/java/br/com/fiap/dao/SetupDAO.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import br.com.fiap.model.Setup;
+import br.com.fiap.model.Usuario;
 import br.com.fiap.utils.EntityManagerFacede;
 
 public class SetupDAO {
@@ -21,9 +22,20 @@ public class SetupDAO {
 	// pegar todos os setups cadastrados no banco
 	public List<Setup> getAll() {
 		EntityManager manager= EntityManagerFacede.getEntityManager();
-		
+
 		String strSQL = "SELECT s FROM Setup s";
 		TypedQuery<Setup> query = manager.createQuery(strSQL, Setup.class);
+		return query.getResultList();
+	}
+	
+	public List<Setup> getOne(Usuario user) {
+		EntityManager manager= EntityManagerFacede.getEntityManager();
+
+		String strSQL = "select s from Setup s where ID_LOGIN_USER =: id";
+		System.out.println(strSQL);
+		TypedQuery<Setup> query = manager.createQuery(strSQL, Setup.class);
+		
+		query.setParameter("id", user.getId());
 		return query.getResultList();
 	}
 
@@ -45,16 +57,17 @@ public class SetupDAO {
 	}
 	
 	
-	public void deleteySetup(Long id) {
-		EntityManager manager= EntityManagerFacede.getEntityManager();
-		
-		manager.getTransaction().begin();
-		Setup setup = manager.find(Setup.class, id);
-		manager.remove(setup);
-		System.out.println("Deletando Setup ID "+setup.getId()+" --- NOME: "+setup.getName());
-		
-		manager.getTransaction().commit();
-		manager.close();
-	}
+	public void deleteSetup(Setup setup) {
+        EntityManager manager= EntityManagerFacede.getEntityManager();
+        
+        manager.getTransaction().begin();
+        setup = manager.merge(setup);
+        manager.remove(setup);
+        manager.flush();
+        System.out.println("Deletando Setup ID "+setup.getId()+" --- NOME: "+setup.getName());
+        
+        manager.getTransaction().commit();
+        manager.close();
+    }
 	
 }
